1eafca430243e65cd1fa1f7c8da789269bfe413e
phukon <rikiphukon16@gmail.com>
2023-11-15 19:48:54 +0530

    Refactored code to improve performance and readability.

---
+++ website/js/commands.js
@@ -0,0 +1,85 @@
+//-----------------
+var twitter = "https://www.twitter.com/kungfukon/";
+var behance = "https://www.behance.net/rikiphukon";
+var password = "adaboost";
+var linkedin = "https://www.linkedin.com/in/riki-phukon/";
+var github = "https://github.com/phukon/";
+var email = 'mailto:hello@rikiphukon.com';
+
+//-----------------
+var citysort = "https://citysort.vercel.app/";
+var prjAth = "https://project-athena-sigma.vercel.app/";
+var prjAthRCT = "https://project-athena-react.vercel.app/";
+var xongroh = "https://www.xongroh.com/";
+
+//-----------------
+
+whois = [
+  "<br>",
+  "Hey, I'm Riki!👋",
+  "I'm a design-centric software developer and who builds engaging websites like this one",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Idk.... do you know?",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  "↘type the name to open the link...",
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/kungfukon' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/riki-phukon' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/phukon' + "</a>",
+  'behance        <a href="' + behance + '" target="_blank">behance.net/rikiphukon' + "</a>",
+  "<br>"
+];
+
+projectsList = [
+  "<br>",
+  'CitySort                       <a href="' + citysort + '" target="_blank">citysort.vercel.app' + '</a>',
+  'Xongroh                        <a href="' + xongroh + '" target="_blank">xongroh.com' + "</a>",
+  'Project Athena(React)          <a href="' + prjAthRCT + '" target="_blank">project-athena-react.vercel.app' + "</a>",
+  'Project Athena                 <a href="' + prjAth + '" target="_blank">project-athena-sigma.vercel.app' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Riki?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Not a pseudo terminal</span>',
+  "__________ ___ ____  /\\ ___ __________  /\\   /\\  ____ ___ ____  /\\________   ______ |\\ ",
+  "\\______   \\   |    |/  \\   |\\______   \\/  |_|  \\|    |   \\    |/  \\_____  \\  \\     \\| |",
+  " |       _/   |       /|   | |     ___/         \\    |   /       / /   |   \\ /   |    |",
+  " |    |   \\   |    |  \\|   | |    |   \\    _    /    |  /|    |  \\/    |    \\    |\\   |",
+  " |____|_  /___|____|__ \\___| |____|    \\  | |  /|______/ |____|__ \\_______  /____| \\  /",
+  "        \\/            \\/                \\/   \\/                  \\/       \\/        \\/",
+
+
+  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
