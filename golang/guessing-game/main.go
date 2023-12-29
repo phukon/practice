b224436d6bab96354426abe95f3815a9c608430e
phukon <hello@rikiphukon.com>
2023-12-29 23:44:19 +0530

    feat: complete game

--- golang/guessing-game/main.go
+++ golang/guessing-game/main.go
@@ -3,21 +3,48 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	secret := getRandomNumber()
-	fmt.Println(secret)
+    // Seed the random number generator
+    rand.Seed(time.Now().UnixNano())
 
-	// get user input
-	guess := getUserInput()
-}
+    // Generate a random number between 1 and 100
+    randomNumber := rand.Intn(100) + 1
 
-func getUserInput() int {
-	fmt.Print("please enter a number")
-	fmt.Scan()
-}
+    // Initialize variables
+    var guess int
+    attempts := 0
+    maxAttempts := 5
 
-func getRandomNumber() int {
-	return rand.Int()%11
+    fmt.Println("Welcome to the Guessing Game!")
+    fmt.Println("Try to guess the number between 1 and 100.")
+
+    // Game loop
+    for {
+        fmt.Print("Enter your guess: ")
+        _, err := fmt.Scanf("%d", &guess)
+        if err != nil {
+            fmt.Println("Please enter a valid number.")
+            continue
+        }
+
+        // Check if the guess is correct
+        if guess < randomNumber {
+            fmt.Println("Too low! Try again.")
+        } else if guess > randomNumber {
+            fmt.Println("Too high! Try again.")
+        } else {
+            fmt.Println("Congratulations! You guessed the correct number!")
+            break
+        }
+
+        attempts++
+        if attempts >= maxAttempts {
+            fmt.Println("Sorry, you've reached the maximum number of attempts.")
+            fmt.Printf("The number was %d.\n", randomNumber)
+            break
+        }
+    }
 }
